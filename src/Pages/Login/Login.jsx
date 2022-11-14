e40c928eb2e7b8ca9dfc4ba3ccdf430b478d34af
NahidulNoman <nahidulna5@gmail.com>
2022-11-14 12:15:58 +0600

    signup and login error handleing using react hook form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,25 +1,36 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const {register,handleSubmit, formState:{errors}} = useForm();
 
-    
+    const handlerLogin = (data) => {
+        console.log(data);
+    }
 
   return (
     <div className="flex justify-center items-center mt-10 mx-5">
       <div className="w-96 shadow-lg p-4 rounded-md">
         <h3 className="text-xl text-center font-bold m-6">Log In</h3>
-        <form>
+        <form onSubmit={handleSubmit(handlerLogin)}>
 
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text font-semibold">Email</span>
             </label>
             <input
-              type="text"
+              type="email"
+              required
+              {...register('email', {
+                required : 'email is required'
+              })}
               placeholder="Email address"
               className="input input-bordered w-full"
             />{" "}
+            {
+                errors.email && <p className="text-red-700 text-sm">{errors.email?.message}</p>
+            }
           </div>
 
           <div className="form-control w-full">
@@ -27,13 +38,21 @@ const Login = () => {
               <span className="label-text font-semibold">Password</span>
             </label>
             <input
-              type="text"
+              type="password"
+              {...register('password', {
+                required : 'password is required'
+              })}
               placeholder="Your password"
               className="input input-bordered w-full"
             />{" "}
+            {
+                errors.password && <p className="text-red-700 text-sm m-1">{errors.password?.message}</p>
+            }
           </div>
-
-          <button className="btn btn-accent w-full mt-4">log In</button>
+          <label className="label">
+              <span className="label-text text-sm ">Forgot Password ?</span>
+            </label>
+          <input className='btn btn-accent w-full' value="Login" type="submit" />
           <p className="text-sm text-center mt-3">
             New Doctor Portals{" "}
             <Link className="text-secondary" to="/signUp">
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,21 +1,36 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+    const {register,handleSubmit,formState:{errors}} = useForm();
+
+    const handlerSingUp = data => {
+        console.log(data);
+    }
+
   return (
     <div className="flex justify-center items-center mx-5 mt-10">
       <div className="w-96 shadow-lg p-4 rounded-lg">
         <h3 className="text-xl text-center font-bold m-6">Sign Up</h3>
-        <form>
+        <form onSubmit={handleSubmit(handlerSingUp)}>
+
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text font-semibold">Your Name</span>
             </label>
             <input
               type="text"
+              required
+              {...register('name', {
+                required : 'name is required'
+              })}
               placeholder="Your name"
               className="input input-bordered w-full"
             />{" "}
+            {
+                errors.name && <p className="text-sm text-red-700">{errors.name?.message}</p>
+            }
           </div>
 
           <div className="form-control w-full ">
@@ -23,10 +38,17 @@ const SignUp = () => {
               <span className="label-text font-semibold">Email Address</span>
             </label>
             <input
-              type="text"
+              type="email"
+              required
+              {...register('email', {
+                required : 'email is required'
+              })}
               placeholder="Email address"
               className="input input-bordered w-full "
             />{" "}
+            {
+                errors.email && <p className="text-sm text-red-700">{errors.email?.message}</p>
+            }
           </div>
 
           <div className="form-control w-full ">
@@ -34,16 +56,21 @@ const SignUp = () => {
               <span className="label-text font-semibold">Your Password</span>
             </label>
             <input
-              type="text"
+              type="password"
+              {...register('password',{
+                required : 'password is required',
+                minLength: { value: 6, message: 'Password must be 6 characters or longer' },
+                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$%&*])(?=.*[0-9])/, message: 'Password must have uppercase, number and special characters' }
+              })}
               placeholder="Your password"
               className="input input-bordered w-full"
             />{" "}
-            <label className="label">
-              <span className="label-text text-sm ">Forgot Password ?</span>
-            </label>
+            {
+                errors.password && <p className="text-sm text-red-700">{errors.password?.message}</p>
+            }
           </div>
 
-          <button className="btn btn-accent w-full mt-3">Sign Up</button>
+          <input className='btn btn-accent w-full mt-4' value="Sign up" type="submit" />
           <p className="text-sm text-center mt-4">Have an Account <Link to='/login' className="text-secondary">Please Log In ?</Link></p>
           <div className="divider">OR</div>
           <button className="btn btn-outline w-full">
