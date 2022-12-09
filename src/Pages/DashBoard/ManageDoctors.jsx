fb9f4fdac7cb1de162868b4aaea05d79c55df05b
NahidulNoman <nahidulna5@gmail.com>
2022-12-09 14:33:10 +0600

    manage doctors page

--- src/Pages/DashBoard/ManageDoctors.jsx
+++ src/Pages/DashBoard/ManageDoctors.jsx
@@ -4,43 +4,50 @@ import toast from "react-hot-toast";
 import ConfirmationModal from "../Share/ConfirmationModal/ConfirmationModal";
 
 const ManageDoctors = () => {
-    const [deletingDoctor, setDeletingDoctor] = useState(null);
+  const [deletingDoctor, setDeletingDoctor] = useState(null);
 
-    const closeModal = () => {
-        setDeletingDoctor(null);
-    };
+  const closeModal = () => {
+    setDeletingDoctor(null);
+  };
 
-  const { data: doctors , refetch} = useQuery({
+  const { data: doctors, refetch } = useQuery({
     queryKey: ["doctors"],
     queryFn: async () => {
       try {
-        const res = await fetch("https://doctors-portal-server-liart.vercel.app/doctors", {
-          headers: {
-            authorization: `bearer ${localStorage.getItem("accessToken")}`,
-          },
-        });
+        const res = await fetch(
+          "https://doctors-portal-server-liart.vercel.app/doctors",
+          {
+            headers: {
+              authorization: `bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
+        );
         const data = await res.json();
         return data;
       } catch (error) {}
     },
   });
-//   console.log(doctors);
+  //   console.log(doctors);
 
-  const handleDeleteDoctor = doctor => {
+  const handleDeleteDoctor = (doctor) => {
     // console.log(id);
-    fetch(`https://doctors-portal-server-liart.vercel.app/doctors/${doctor._id}`, {
-        method : 'delete',
-        headers : {
-            authorization: `bearer ${localStorage.getItem("accessToken")}`
-        }})
-        .then(res => res.json())
-        .then(data => {
-                    if(data.deletedCount > 0){
-                    toast.success('Doctor delete successfully.')
-                    refetch();
-                }
-        })
-  }
+    fetch(
+      `https://doctors-portal-server-liart.vercel.app/doctors/${doctor._id}`,
+      {
+        method: "delete",
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          toast.success("Doctor delete successfully.");
+          refetch();
+        }
+      });
+  };
 
   return (
     <div>
@@ -51,8 +58,7 @@ const ManageDoctors = () => {
         <table className="table w-full">
           <thead>
             <tr>
-              <th>
-              </th>
+              <th></th>
               <th>AVATAR</th>
               <th>NAME</th>
               <th>SPECIALTY</th>
@@ -60,50 +66,49 @@ const ManageDoctors = () => {
             </tr>
           </thead>
           <tbody>
-            {
-                doctors?.map((doctor,i) => <tr key={doctor._id}>
-                    <th>
-                      <label>
-                        {i+1}
-                      </label>
-                    </th>
-                    <td>
-                      <div className="flex items-center space-x-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img
-                              src={doctor.image}
-                              alt="Avatar Tailwind CSS Component"
-                            />
-                          </div>
-                        </div>
+            {doctors?.map((doctor, i) => (
+              <tr key={doctor._id}>
+                <th>
+                  <label>{i + 1}</label>
+                </th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={doctor.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
                       </div>
-                    </td>
-                    <td>
-                      {doctor.doctorName}
-                    </td>
-                    <td>{doctor.specialty}</td>
-                    <th>
-                      <label 
-                      htmlFor="confirmation"
-                      onClick={() => setDeletingDoctor(doctor)}
-                      className="btn btn-primary bg-red-600 text-white btn-xs">DELETE</label>
-                    </th>
-                  </tr>)
-            }
+                    </div>
+                  </div>
+                </td>
+                <td>{doctor.doctorName}</td>
+                <td>{doctor.specialty}</td>
+                <th>
+                  <label
+                    htmlFor="confirmation"
+                    onClick={() => setDeletingDoctor(doctor)}
+                    className="btn btn-primary bg-red-600 text-white btn-xs"
+                  >
+                    DELETE
+                  </label>
+                </th>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
-      {
-        deletingDoctor && <ConfirmationModal
-        title={`Are you sure you want to delete?`}
-        message={`If you delete ${deletingDoctor.doctorName}. It cannot be undone.`}
-        successAction = {handleDeleteDoctor}
-        successButtonName="Delete"
-        modalData = {deletingDoctor}
-        closeModal = {closeModal}
+      {deletingDoctor && (
+        <ConfirmationModal
+          title={`Are you sure you want to delete?`}
+          message={`If you delete ${deletingDoctor.doctorName}. It cannot be undone.`}
+          successAction={handleDeleteDoctor}
+          successButtonName="Delete"
+          modalData={deletingDoctor}
+          closeModal={closeModal}
         ></ConfirmationModal>
-      }
+      )}
     </div>
   );
 };
