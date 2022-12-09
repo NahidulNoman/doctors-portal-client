bd2593450a82f6e8ad374189bcc332ad83700f4c
NahidulNoman <nahidulna5@gmail.com>
2022-12-09 17:25:15 +0600

    admin route

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,21 +1,21 @@
-import React, {  useContext } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import useAdmin from '../hooks/useAdmin';
-import { AuthContext } from '../UserContext/UserContext';
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
+import { AuthContext } from "../UserContext/UserContext";
 
-const AdminRoute = ({children}) => {
-    const {user , loading} = useContext(AuthContext);
-    const [isAdmin,isAdminLoading] = useAdmin(user?.email);
-    const location = useLocation();
+const AdminRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const [isAdmin, isAdminLoading] = useAdmin(user?.email);
+  const location = useLocation();
 
-    if(loading || isAdminLoading){
-        return <progress className="progress w-56"></progress>
-    }
+  if (loading || isAdminLoading) {
+    return <progress className="progress w-56"></progress>;
+  }
 
-    if(user && isAdmin){
-        return children;
-    }
-    return <Navigate to='/login' state={{from : location}} replace ></Navigate>
+  if (user && isAdmin) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default AdminRoute;
